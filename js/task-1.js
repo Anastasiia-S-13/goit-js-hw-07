97d65c177243251c72edac820f50d41c105a7fe1
Anastasiia <gorgona1597531597@gmail.com>
2025-06-22 16:51:01 +0200

    update 1 and 3 tasks

--- js/task-1.js
+++ js/task-1.js
@@ -2,62 +2,39 @@ const list = document.querySelector("#categories");
 const listItem = document.querySelector(".item")
 
 const listLength = list.querySelectorAll(".item");
+list.classList.add("big-list");
 
-
-const firstChild = document.querySelector("#categories").childNodes[1];
-const firstTitle = firstChild.querySelector("h2")
-const firstTitleContext = firstChild.querySelector("h2").textContent;
-const firstList = firstChild.querySelector("ul");
-
-const firstListLength = firstChild.querySelectorAll("ul li");
-
-
-const secondChild = document.querySelector("#categories").childNodes[3];
-const secondTitle = secondChild.querySelector("h2")
-const secondTitleContext = secondChild.querySelector("h2").textContent;
-const secondList = secondChild.querySelector("ul");
-
-const secondListLength = secondChild.querySelectorAll("ul li");
-
-
-const thirdChild = document.querySelector("#categories").childNodes[5];
-const thirdTitle = thirdChild.querySelector("h2");
-const thirdTitleContext = thirdChild.querySelector("h2").textContent;
-const thirdList = thirdChild.querySelector("ul");
-
-const thirdListLength = thirdChild.querySelectorAll("ul li");
-
-
-function totalElements(items) {
+function totalElements(params) {
   let total = 0;
 
-    items.forEach(() => {
-    total += 1;
+    params.forEach(() => {
+      total += 1;
   });
 
   return total;
 }
 
-
-
 console.log(`Number of categories: ${totalElements(listLength)}`);
 
-console.log(`Category: ${firstTitleContext}`);
-console.log(`Elements: ${totalElements(firstListLength)}`);
+function elementsInLists(items) {
+  items.forEach((item) => {
+    item.classList.add("categories-items");
 
-console.log(`Category: ${secondTitleContext}`);
-console.log(`Elements: ${totalElements(secondListLength)}`);
+    const name = item.querySelector("h2");
+    name.classList.add("title");
+    console.log(`Category: ${name.textContent}`);
 
-console.log(`Category: ${thirdTitleContext}`);
-console.log(`Elements: ${totalElements(thirdListLength)}`);
+    const listInside = item.querySelector("ul");
+    listInside.classList.add("list-items");
 
-list.classList.add("big-list");
-listItem.classList.add("categories-items");
-secondChild.classList.add("categories-items");
-thirdChild.classList.add("categories-items");
-firstTitle.classList.add("title");
-secondTitle.classList.add("title");
-thirdTitle.classList.add("title");
-firstList.classList.add("list-items");
-secondList.classList.add("list-items");
-thirdList.classList.add("list-items");
+    const itemsInside = listInside.querySelectorAll("li");
+    console.log(`Elements: ${totalElements(itemsInside)}`);
+  })
+  
+}
+
+elementsInLists(listLength);
+
+
+
+
--- js/task-3.js
+++ js/task-3.js
@@ -2,14 +2,13 @@ const userName = document.querySelector("#name-input");
 userName.addEventListener("input", addUserName);
 
 const resultText = document.querySelector("#name-output");
-const defaultText = resultText.textContent;
 
 function addUserName(event) {
     const userInput = event.target.value;
     if (userInput.trim() !== "") {
         resultText.textContent = userInput.trim();
     } else {
-        return resultText.textContent = defaultText;
+        resultText.textContent = "Anonymous";
     }
 }
 
